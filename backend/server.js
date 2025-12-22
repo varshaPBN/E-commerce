2a126b340785c4dd3d1ef4875a1e8bf47bc5c4f8
Sabeeha KT <kampalasabeeha@gmail.com>
2025-12-22 10:26:21 +0530

    MongoDB Connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express"); // npm i express
+const mongoose = require("mongoose"); //npm i mongoose
+require("dotenv").config(); // Load environment variables. Make sure .env is in .gitignore
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});  
