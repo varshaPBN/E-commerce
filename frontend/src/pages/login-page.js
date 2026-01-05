bb768a95ee96ec86d5e9f34813f415d17a1c94fd
varshaPBN <varshanair1666@gmail.com>
2026-01-05 19:13:29 +0530

    login and user-login pages frontend

---
+++ frontend/src/pages/login-page.js
@@ -0,0 +1,12 @@
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import Login from "@/components/Login";
+import theme from "@/theme/theme";
+
+export default function LoginPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Login />
+    </ThemeProvider>
+  );
+}
